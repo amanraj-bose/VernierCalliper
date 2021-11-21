8640f09814c7a4fe23ae269fc5e583599b03db9f
amanraj-bose <88227750+amanraj-bose@users.noreply.github.com>
2021-11-21 20:33:34 +0530

    Create install.ps1

---
+++ Install/install.ps1
@@ -0,0 +1,11 @@
+cls
+pip install --upgrade pip
+pip install requests
+pip install pyttsx3
+pip install tkinter
+pip install math
+python --version
+g++
+start-process setup_espeak-1.48.04
+cls
+exit
