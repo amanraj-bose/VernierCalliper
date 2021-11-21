18bfee78e88505cddfad078bc551091aadd0ab6f
amanraj-bose <88227750+amanraj-bose@users.noreply.github.com>
2021-11-21 20:37:10 +0530

    Create install_Termux.sh

---
+++ Install/install_Termux.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+clear
+apt install python3
+pip3 install math
+pip3 install requests
+apt install figlet -y
+apt install ruby -y
+gem install lolcat
+chmod +777 Install_Exe
+chmod +777 Main_Exe_File
+chmod +777 main_c++
+chmod +777 main_Python
+chmod +777 Exe_File
+apt update && apt upgrade -y
+pip3 install --upgrade pip
+clear
+figlet -c Completed | lolcat
+echo
+echo
+echo
+echo "$(tput setaf 2)"Completed ..."$(tput sgr0)"
+exit
