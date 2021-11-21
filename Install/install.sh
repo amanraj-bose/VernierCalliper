2e563c7a5d1c9a528c9755473c3ca1372fc79ed7
amanraj-bose <88227750+amanraj-bose@users.noreply.github.com>
2021-11-21 20:36:51 +0530

    Create install.sh

---
+++ Install/install.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+clear
+sudo apt update -y
+sudo apt upgrade -y
+sudo apt install clang -y
+sudo apt install python3 -y
+sudo apt install figlet -y
+sudo apt install figlet | lolcat -y
+pip3 install math
+pip3 install requests
+pip install espeak -y
+pip3 install pyttsx3
+pip3 install tkinter
+pip3 install --upgrade pip
+chmod +777 Install_Exe
+chmod +777 Main_Exe_File
+chmod +777 main_c++
+chmod +777 main_Python
+chmod +777 Exe_File
+sudo apt install g++ -y
+sudo apt install build-essential -y
+clear
+figlet -c Completed | lolcat
+echo
+echo
+echo
+echo "$(tput setaf 2)"Completed ..."$(tput sgr0)"
+exit
+
