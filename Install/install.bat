e5d5efc494a0b27e3b44912196333b3aa871008d
amanraj-bose <88227750+amanraj-bose@users.noreply.github.com>
2021-11-21 20:32:59 +0530

    Create install.bat

---
+++ Install/install.bat
@@ -0,0 +1,12 @@
+cls
+pip install --upgrade pip
+pip install requests
+pip install pyttsx3
+pip install tkinter
+pip install math
+python --version
+g++
+python espeak.py
+cls
+C:\Users\AMAN\Documents\GitHub\Vernier_Calliper\Install_Exe\setup_espeak-1.48.04
+exit
