d31388091a7cba67f317b9970f78b8aeadc64280
amanraj-bose <88227750+amanraj-bose@users.noreply.github.com>
2021-11-24 16:32:40 +0530

    Update install.bat

--- Install/install.bat
+++ Install/install.bat
@@ -4,6 +4,9 @@ pip install requests
 pip install pyttsx3
 pip install tkinter
 pip install math
+pip install pyinstaller
+pip uninstall typing
+pyinstaller --onefile main_Windows.py
 python --version
 g++
 python espeak.py
