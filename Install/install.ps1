cf5eeacdd421de0543174a44370f6d98df8d07d1
amanraj-bose <88227750+amanraj-bose@users.noreply.github.com>
2021-11-24 16:34:06 +0530

    Update install.ps1

--- Install/install.ps1
+++ Install/install.ps1
@@ -4,6 +4,10 @@ pip install requests
 pip install pyttsx3
 pip install tkinter
 pip install math
+pip install pyinstaller
+pip uninstall typing
+pyinstaller --onefile main_Windows.py
+pip install typing
 python --version
 g++
 cls
